2de1c07444a1b1a7799512922a0a656361f72a69
Adrien Coudour <adrien.coudour@etu.uca.fr>
2021-12-13 09:47:00 +0100

    Suite detection image avec Ugo

--- code/src/view/selection_view.java
+++ code/src/view/selection_view.java
@@ -16,6 +16,7 @@ import javax.sound.midi.Soundbank;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Handler;
 
 public class selection_view {
@@ -25,14 +26,24 @@ public class selection_view {
 
     String idTabSelected = "tab1";
 
+    private List<Image> image;
+
+    private double largeur;
+
     @FXML
     public void initialize(){
         int cpt = 1;
-
+        Decoupeur d = new Decoupeur();
         File f = new File(System.getProperty("user.dir")+"/ressources/Images");
         var truc = Arrays.asList(f.list());
         ScrollPane sp;
         for(var file : truc){
+            File tileset = new File (System.getProperty("user.dir")+"/ressources/Images/"+file);
+            Image taille = new Image(String.valueOf(tileset));
+            double largeurImage = taille.getWidth()/32;
+            double hauteurImage = taille.getHeight()/32;
+            image = d.decoupe(tileset.getAbsolutePath(),(int)largeurImage,(int)hauteurImage);
+            System.out.println(tileset);
             sp = new ScrollPane(new ImageView(new Image(f+"\\"+file)));
 
             sp.setOnMouseClicked(new EventHandler<MouseEvent>() {
@@ -61,7 +72,12 @@ public class selection_view {
     public void positionclic(MouseEvent mouseEvent) {
         System.out.println("Tab numéro : " + idTabSelected);
         System.out.println("Numéro de Tuile en X : " + (int)mouseEvent.getX()/32);
-        System.out.println("Numéro de Tuile en Y : " + (int)mouseEvent.getY()/32);
+        System.out.println("Numéro de Tuile en Y : " + (int) mouseEvent.getY()/32);
+        //System.out.println("Liste des tuiles : " + image);
+        Decoupeur d=new Decoupeur();
+
+        int numero= (int)mouseEvent.getX()/32+((int)largeur+(int)mouseEvent.getY()/32);
+        System.out.println(image.get(numero));
     }
 
 
